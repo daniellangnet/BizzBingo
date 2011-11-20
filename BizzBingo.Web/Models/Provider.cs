61ff85dcd24946d9feb80dd8e9b8b494ce698c4c
Robert Muehsig <robert.muehsig@code-developer.de>
2011-11-20 15:49:48 +0100

    New versions of RavenDB/jQuery... + minor changes in footer

--- BizzBingo.Web/Models/Provider.cs
+++
@@ -1,9 +0,0 @@
-namespace BizzBingo.Web.Models
-{
-    public enum Provider
-    {
-        Website = 0,
-        Speakersdeck = 1,
-        YouTube = 2
-    }
-}
--- BizzBingo.Web/Models/Type.cs
+++
@@ -1,9 +0,0 @@
-namespace BizzBingo.Web.Models
-{
-    public enum Type
-    {
-        Website = 0,
-        Presentation = 1,
-        Video = 2
-    }
-}
